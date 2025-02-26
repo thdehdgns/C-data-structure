437a000e4429d0cfee91868a862dc74d3c607f50
DongHoon <ehdgnsdlrk@gmail.com>
2025-02-26 19:42:51 +0900

    2025-02-26 벡터

--- Class 6th (Circle Queue)/Class 6th (Circle Queue).cpp
+++ Class 6th (Circle Queue)/Class 6th (Circle Queue).cpp
@@ -1,7 +1,17 @@
 ﻿#include <iostream>
+#include "Vector.h"
 using namespace std;
 int main()
 {
-    
+    Vector<int> vector;
+    vector.push_back(10);
+    vector.push_back(20);
+    vector.push_back(30);
+    vector.pop_back();
+
+    for (int i = 0; i < vector.Size(); i++)
+    {
+        cout << vector[i] << endl;
+    }
     return 0;
 }
--- Class 6th (Circle Queue)/Vector.h
+++ Class 6th (Circle Queue)/Vector.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+using namespace std;
 template<typename T>
 class Vector
 {
@@ -17,12 +19,82 @@ public:
 
 	void push_back(T data)
 	{
-		
+		if (capacity <= 0)
+		{
+			resize(1);
+		}
+		else if(size >= capacity)
+		{
+			resize(capacity * 2);
+		}
+		container[size++] = data;
+
+
 	}
 
+	void pop_back()
+	{
+		if (size <= 0)
+		{
+			cout << "Vector is empty" << endl;
+		}
+		else
+		{
+			container[size--] == NULL;
+			
+
+		}
+	}
+
+	const int& Size()
+	{
+		return size;
+	}
+
+	const T & operator[](const int& index)
+	{
+		return container[index];
+	}
 	void resize(int newSize)
 	{
+		// 1.cpapacity에 새로운 size 값을 저장합니다.
+		capacity = newSize;
+		
+		// 2. 새로운 포인트 변수를 생성해서 새롭게 만들어진
+		//    메모리 공간을 가리키도록 합니다.
+		T* newcontainer = new T[capacity];
+		// 3. 새로운 메모리 공간의 값을 초기화합니다.
+		for (int i = 0; i < capacity; i++)
+		{
+			newcontainer[i] = NULL;
+		}
+		// 4. 기존 배열에 있는 값을 복사해서 새로운 배열에
+		//    넣어줍니다.
+		
+		for (int i = 0; i < size; i++)
+		{
+			newcontainer[i] = container[i];
+		}
 
+		// 5. 기존 배열의 메모리를 해제합니다.
+		 
+		if (container != nullptr)
+		{
+			delete[] container;
+		}
+		// 6. 기존의 배열을 가리키던 포인터 변수의 값을
+		//    새로운 배열의 시작 주소로 가리킵니다.
+		
+		container = newcontainer;
 	}
+
+	~Vector()
+	{
+		if (container != nullptr)
+		{
+			delete[] container;
+ 		}
+	}
+
 };
 
