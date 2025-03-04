5829e7553e4cda00d5e66cc35dd0af95426213f4
DongHoon <ehdgnsdlrk@gmail.com>
2025-03-04 19:43:58 +0900

    2025-03-04 이진탐색트리

--- Class 8th(String)/Class 8th(String).cpp
+++ Class 8th(String)/Class 8th(String).cpp
@@ -1,7 +1,12 @@
 ﻿#include <iostream>
+#include "HashTable.h"
 using namespace std;
 int main()
 {
+    HashTable<const char*, int> hashtable;
+
+    hashtable.Insert("Sowrd", 10000);
+    hashtable.Insert("Armor", 4000);
 
 
     return 0;
--- Class 8th(String)/HashTable.h
+++ Class 8th(String)/HashTable.h
@@ -6,55 +6,117 @@ template <typename KEY, typename VALUE>
 class HashTable
 {
 private:
-	struct Node
-	{
-		KEY* key;
-		VALUE* value;
-		Node* next;
-	};
+    struct Node
+    {
+        KEY key;
+        VALUE value;
 
-	struct Bucket
-	{
-		int count;
-		Node* head;
-	};
+        Node* next;
+    };
 
-	Bucket bucket[SIZE];
+    struct Bucket
+    {
+        int count;
+        Node* head;
+    };
 
+    Bucket bucket[SIZE];
 public:
+    HashTable()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            bucket[i].count = 0;
+            bucket[i].head = nullptr;
+        }
+    }
 
-	HashTable()
-	{
-		for (int i = 0; i < SIZE; i++)
-		{
-			buket[i].count = 0;
-			buket[i].head = nullptr;
+    template <typename T>
+    const int& HashFunction(T key)
+    {
+        unsigned int hashIndex = (int)key % SIZE;
 
-		}
-	}
+        return hashIndex;
+    }
 
-	template<typename T>
-	Function(T data)
-	{
-		unsigned int hashIndex = (int)key % SIZE;
-		return hashIndex;
-	}
+    template<>
+    const int& HashFunction(const char* key)
+    {
+        int hash = 0;
 
-	template<>
-	const int& Function(const char* key)
-	{
-		int hash = 0;
+        for (int i = 0; i < strlen(key); i++)
+        {
+            hash += key[i];
+        }
 
-		for (int i = 0; i < strlen(key); i++)
-		{
-			hash += key[i];
+        int hashIndex = hash % SIZE;
 
-		}
+        return hashIndex;
+    }
 
-		int hashIndex = hash % SIZE;
-		return hashIndex;
-	}
+    Node* CreateNode(KEY key, VALUE value)
+    {
+        Node* newNode = new Node;
 
+        newNode->key = key;
+        newNode->value = value;
+        newNode->next = nullptr;
 
+        return newNode;
+    }
+
+    void Insert(KEY key, VALUE value)
+    {
+        // 해시 함수를 통해서 값을 받는 임시 변수
+        int hashIndex = HashFunction(key);
+
+        // 새로운 노드를 생성합니다.
+        Node* newNode = CreateNode(key, value);
+
+        // 노드가 1개라도 존재하지 않는다면
+        if (bucket[hashIndex].count == 0)
+        {
+            // bucket[hashIndex]의 head 포인터가 newNode를 가리키게 합니다. 
+            bucket[hashIndex].head = newNode;
+        }
+        else
+        {
+            newNode->next = bucket[hashIndex].head;
+
+            bucket[hashIndex].head = newNode;
+        }
+
+        // bucket[hashIndex]의 count를 증가시킵니다.
+        bucket[hashIndex].count++;
+    }
+
+    ~HashTable()
+    {
+        
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            Node* deleteNode = bucket[i].head;
+            Node* nextNode = bucket[i].head;
+
+            if (bucket[i].head == nullptr)
+            {
+                continue;
+            }
+            else
+            {
+                while (nextNode != nullptr)
+                {
+                    nextNode = deleteNode->next;
+
+                    delete deleteNode;
+
+                    deleteNode = nextNode;
+                }
+
+            }
+
+        }
+    }
 };
 
