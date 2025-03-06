6e1179ff1701a13cc63a015b4f9e57032b921a31
DongHoon <ehdgnsdlrk@gmail.com>
2025-03-06 19:45:57 +0900

    20250306 레드 블랙 트리

--- Class 9th (Hash Table)/Class 9th (Hash Table).cpp
+++ Class 9th (Hash Table)/Class 9th (Hash Table).cpp
@@ -1,9 +1,21 @@
 ﻿#include <iostream>
-#include <queue>
+#include "PriorityQueue.h"
 using namespace std;
 int main()
 {
+    PriorityQueue<int> priorityQueue;
+    priorityQueue.push(10);
+    priorityQueue.push(5);
+    priorityQueue.push(7);
+    priorityQueue.push(12);
+    priorityQueue.push(3);
     
+
+    while (priorityQueue.empty() == false)
+    {
+        cout << priorityQueue.Top() << endl;
+        priorityQueue.pop();
+    }
 
     return 0;
 }
--- Class 9th (Hash Table)/PriorityQueue.h
+++ Class 9th (Hash Table)/PriorityQueue.h
@@ -6,7 +6,7 @@ template <typename T>
 class PriorityQueue
 {
 private:
-	T* container[SIZE];
+	T container[SIZE];
 	int index;
 
 public:
@@ -21,5 +21,86 @@ public:
 		}
 	}
 
+	void push(T data)
+	{
+
+		if (index + 1 >= SIZE)
+		{
+			cout << "overflow" << endl;
+		}
+		else
+		{
+			container[++index] = data;
+			int child = index;
+			int parent = child/2;
+
+			while (child > 1)
+			{
+				if (container[parent] < container[child])
+				{
+					swap(container[parent], container[child]);
+				}
+
+				child = parent;
+				parent = child / 2;
+			}
+		}
+
+	}
+
+	const T& Top()
+	{
+		return container[1];
+	}
+
+	void pop()
+	{
+		if (index <= 0)
+		{
+			cout << "Priority Queue is Empty" << endl;
+		}
+		else
+		{
+			container[1] = container[index];
+
+			container[index--] = NULL;
+
+			int parent = 1;
+
+			while (parent * 2 <= index)
+			{
+				int child = parent * 2;
+
+				if (container[child] < container[child + 1])
+				{
+					child++;
+				}
+
+				if (container[child] < container[parent])
+				{
+					break;
+				}
+				else
+				{
+					std::swap(container[parent], container[child]);
+
+					parent = child;
+				}
+			}
+		}
+	}
+
+	const bool empty()
+	{
+		if (index <= 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
 };
 
